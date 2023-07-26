7dde71c5aaceecba6b1033a25b101d5f09402826
Karatasdilara <karatassdilara@gmail.com>
2023-07-26 21:18:49 +0300

    a

--- app/src/main/java/com/example/uygulamadersi/soru3/HayvanBarınagı.kt
+++ app/src/main/java/com/example/uygulamadersi/soru3/HayvanBarınagı.kt
@@ -3,7 +3,7 @@ package com.example.uygulamadersi.soru3
 import com.example.uygulamadersi.soru2.MuzikAleti
 
 class HayvanBarınagı {
-    fun ses(Hayvan: Hayvan) {
-        Hayvan.sesCikar()
+    fun sesCikar(hayvan: Hayvan) {
+        hayvan.sesCikar()
     }
 }
--- app/src/main/java/com/example/uygulamadersi/soru3/main.kt
+++ app/src/main/java/com/example/uygulamadersi/soru3/main.kt
@@ -5,4 +5,9 @@ import com.example.uygulamadersi.soru2.MüzikDersi
 fun main (){
     val kedi =Kedi ()
     kedi.sesCikar()
+    val hayvan1: Hayvan = Kedi()
+    val hayvan2: Hayvan = Köpek()
+    val barinak = HayvanBarınagı()
+    barinak.sesCikar(hayvan1)
+    barinak.sesCikar(hayvan2)
 }
