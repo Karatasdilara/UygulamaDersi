fea86d1e9a1ddf4ef04af13af36d0035b373e833
Karatasdilara <karatassdilara@gmail.com>
2023-08-01 20:36:41 +0300

    .

---
+++ app/src/main/java/com/example/uygulamadersi/Hafta2/soru2/soru2.kt
@@ -0,0 +1,11 @@
+package com.example.uygulamadersi.Hafta2.soru2
+
+fun TekSayilar(liste: Array<Int>): List<Int> {
+    return liste.filter { it % 2 != 0 }
+}
+
+fun main() {
+    val myArray = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val sonuc = TekSayilar(myArray)
+    println("Tek sayılar: $sonuc")
+}
