667163e80108ae47c99e88f3ed36606da7bcad6a
Karatasdilara <karatassdilara@gmail.com>
2023-08-01 22:19:30 +0300

    .

--- app/src/main/java/com/example/uygulamadersi/Hafta2/soru9/soru9.kt
+++ app/src/main/java/com/example/uygulamadersi/Hafta2/soru9/soru9.kt
@@ -4,7 +4,7 @@ fun main() {
     val liste1 = listOf(1, 2, 3, 4, 5)
     val liste2 = listOf(3, 4, 5, 6, 7)
 
-    val liste2deOlmayanlar = liste1.filterNot { it in liste2 }
+    val sonuc = liste1.minus(liste2)
 
-    println("Liste 2'de olmayanlar: $liste2deOlmayanlar")
+    println("Sonuç: $sonuc")
 }
