fd239094cd7d852c21531a2489dd439f4cb02fe9
Karatasdilara <karatassdilara@gmail.com>
2023-08-02 22:06:09 +0300

    .

---
+++ app/src/main/java/com/example/uygulamadersi/Hafta2/soru16/Memur.kt
@@ -0,0 +1,7 @@
+package com.example.uygulamadersi.Hafta2.soru16
+
+class Memur(val maas: Int) : Calisan {
+    override fun maasHesapla(): Int {
+        return maas
+    }
+}
---
+++ app/src/main/java/com/example/uygulamadersi/Hafta2/soru16/Mudur.kt
@@ -0,0 +1,7 @@
+package com.example.uygulamadersi.Hafta2.soru16
+
+class Mudur(val aylikMaas: Int) : Calisan {
+    override fun maasHesapla(): Int {
+        return aylikMaas
+    }
+}
